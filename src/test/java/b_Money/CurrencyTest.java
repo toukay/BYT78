d0e20b4cf8609ca63cc50b07c69414187250bba1
toukay <35756997+toukay@users.noreply.github.com>
2024-01-16 13:17:52 +0100

    Add Currency tests.

--- src/test/java/b_Money/CurrencyTest.java
+++ src/test/java/b_Money/CurrencyTest.java
@@ -7,38 +7,58 @@ import org.junit.Test;
 
 public class CurrencyTest {
 	Currency SEK, DKK, NOK, EUR;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		/* Setup currencies with exchange rates */
 		SEK = new Currency("SEK", 0.15);
 		DKK = new Currency("DKK", 0.20);
+		NOK = new Currency("NOK", 0.35);
 		EUR = new Currency("EUR", 1.5);
 	}
 
 	@Test
 	public void testGetName() {
-		fail("Write test case here");
+		assertEquals("SEK", SEK.getName());
+		assertEquals("DKK", DKK.getName());
+		assertEquals("NOK", NOK.getName());
+		assertEquals("EUR", EUR.getName());
 	}
-	
+
 	@Test
 	public void testGetRate() {
-		fail("Write test case here");
+		assertEquals(0.15, SEK.getRate(), 0.001);
+		assertEquals(0.20, DKK.getRate(), 0.001);
+		assertEquals(0.35, NOK.getRate(), 0.001);
+		assertEquals(1.5, EUR.getRate(), 0.001);
 	}
-	
+
 	@Test
 	public void testSetRate() {
-		fail("Write test case here");
+		SEK.setRate(0.16);
+		DKK.setRate(3.22);
+		NOK.setRate(0.94);
+		EUR.setRate(0.23);
+		assertEquals(0.16, SEK.getRate(), 0.001);
+		assertEquals(3.22, DKK.getRate(), 0.001);
+		assertEquals(0.94, NOK.getRate(), 0.001);
+		assertEquals(0.23, EUR.getRate(), 0.001);
 	}
-	
+
 	@Test
 	public void testGlobalValue() {
-		fail("Write test case here");
+		assertEquals(150, (int)SEK.universalValue(1000)); // e.g. (0.15 * 1000) = 150
+		assertEquals(200, (int)DKK.universalValue(1000));
+		assertEquals(350, (int)NOK.universalValue(1000));
+		assertEquals(1500, (int)EUR.universalValue(1000));
 	}
-	
+
 	@Test
 	public void testValueInThisCurrency() {
-		fail("Write test case here");
+		assertEquals(10000, (int)SEK.valueInThisCurrency(1000, EUR)); // e.g. (1.5 * 1000) / 0.15 = 10000 (1.5 is EUR rate, and 0.15 is SEK rate)
+		assertEquals(7500, (int)DKK.valueInThisCurrency(1000, EUR));
+		assertEquals(4285, (int)NOK.valueInThisCurrency(1000, EUR));
+		assertEquals(100, (int)EUR.valueInThisCurrency(1000, SEK));
 	}
 
 }
